9a2c9c0fec571a10ef27e1afacc5cf72db43a08e
LizardBiterXeBoss-LBXB <3909563767@qq.com>
2026-06-21 15:06:51 +0800

    1.0.0Beta
    LizardSystemAPIonGithub

---
+++ LizardApiOnGit.hpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<fstream>
+#include<cstdlib>
+
+#program ls-api-vone
+#define VERSION 1.0.0Beta
+
+class ls_api_1{
+  std::string temp_string="",slast="";
+  int start=0,end=0; 
+  public:
+    inline void print(string temp_string,string slast){
+      std::cout<<temp_string<<slast;
+      return;
+    }
+    inline static long long count(int start,int end){
+      for(int i=start;i<=end;i++){
+        return i;
+      }
+    }
+};
